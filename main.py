24dcf35736e64ced214f2168313cf57e1a1b76d4
VincentLi1216 <sunnus.tw@gmail.com>
2024-02-18 18:08:49 +0800

    makedirs for main.py

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ from util_written_mask import create_written_mask
 from util_conv_array import conv_array, diff_array, filter_np_array
 
 
+def main(video_path):
+    v_basename = os.path.basename(video_path)
+    v_basename, _ = os.path.splitext(v_basename)
+    dir_path = os.path.join("./cache", v_basename)
+    print(f'Working dir: "{dir_path}"')
+    os.makedirs(dir_path, exist_ok=True)
+
+if __name__ == "__main__":
+    main("./mp4_videos/example1.mp4")
